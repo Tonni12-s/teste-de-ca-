0b219aefc55258f97be76106f8bec32cf771401d
Tonni henrique <tonnihenrique239@gmail.com>
2024-12-07 18:56:05 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# import PyQt5
+# from PyQt5 import uic, QtWidgets
+# app = QtWidgets.QApplication([])
+#
+# def exibir_mensagem():
+#     print("Botão clicado")
+#
+# tela = uic.loadUi("tela1.ui")
+#
+# tela.pushButton.clicked.connect(exibir_mensagem)
+#
+# tela.show()
+# app.exec()
+#
